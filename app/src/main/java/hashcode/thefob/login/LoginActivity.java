828263432dad7d0d15978d236ce723dfd3e9e90d
Nipun Sampath <nipunsampath@gmail.com>
2018-09-08 06:44:10 +0530

    layout changes

--- app/src/main/java/hashcode/thefob/login/LoginActivity.java
+++ app/src/main/java/hashcode/thefob/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package hashcode.thefob;
+package hashcode.thefob.login;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -31,6 +31,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import hashcode.thefob.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
