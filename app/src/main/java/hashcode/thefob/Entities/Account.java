39b640f5cb5b374a9128f4609b7c6b6678ad7f69
PraveenRamanayake <rmnpbr@gmail.com>
2018-09-14 00:30:43 +0530

    created entity classes

---
+++ app/src/main/java/hashcode/thefob/Entities/Account.java
@@ -0,0 +1,42 @@
+package hashcode.thefob.Entities;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+public class Account {
+    private String accountId;
+    private String accountName;
+    private Hashtable<String, String> accountDetails
+            = new Hashtable<String, String>();
+
+    public Account(String accountId, String accountName, Hashtable<String, String> accountDetails) {
+        this.accountId = accountId;
+        this.accountName = accountName;
+        this.accountDetails = accountDetails;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public Hashtable<String, String> getAccountDetails() {
+        return accountDetails;
+    }
+
+    public void setAccountDetails(Hashtable<String, String> accountDetails) {
+        this.accountDetails = accountDetails;
+    }
+}
---
+++ app/src/main/java/hashcode/thefob/Entities/AccountType.java
@@ -0,0 +1,66 @@
+package hashcode.thefob.Entities;
+
+
+import android.media.Image;
+
+import java.util.ArrayList;
+
+public class AccountType {
+
+    private String accountTypeName;
+
+    private String accountTypeId;
+
+    // Drawable resource ID for the icon
+    private int iconId;
+
+    //Accounts in a particular account type
+    private ArrayList<Account> accounts=new ArrayList<Account>();
+
+
+
+    public AccountType(String accountTypeName, String accountTypeId, int iconId) {
+        this.accountTypeName = accountTypeName;
+        this.accountTypeId = accountTypeId;
+        this.iconId = iconId;
+    }
+
+    public AccountType(String accountTypeName, String accountTypeId, int iconId, ArrayList<Account> accounts) {
+        this.accountTypeName = accountTypeName;
+        this.accountTypeId = accountTypeId;
+        this.iconId = iconId;
+        this.accounts = accounts;
+    }
+
+    public String getAccountTypeName() {
+        return accountTypeName;
+    }
+
+    public void setAccountTypeName(String accountTypeName) {
+        this.accountTypeName = accountTypeName;
+    }
+
+    public String getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(String accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public void setIconId(int iconId) {
+        this.iconId = iconId;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
