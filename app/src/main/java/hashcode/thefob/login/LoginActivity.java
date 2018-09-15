dba54a0535b875f50a4262c8196d054329697b4d
Nipun Sampath <nipunsampath@gmail.com>
2018-09-15 22:36:13 +0530

    added animation to login activity

--- app/src/main/java/hashcode/thefob/login/LoginActivity.java
+++ app/src/main/java/hashcode/thefob/login/LoginActivity.java
@@ -1,11 +1,19 @@
 package hashcode.thefob.login;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import net.sqlcipher.database.SQLiteDatabase;
 
 import hashcode.thefob.MenuActivity;
@@ -42,6 +50,30 @@ public class LoginActivity extends AppCompatActivity
 
     }
 
+    public void login(View v)
+    {
+        TextView passwordInput = findViewById(R.id.password_input_login);
+        PASSWORD = passwordInput.getText().toString();
+        try
+        {
+            SQLiteDatabase db = SQLiteDatabaseHelper.connectDataBase(this,PASSWORD);
+            nextActivity();
+
+        }
+        catch(net.sqlcipher.database.SQLiteException e)
+        {
+            View contextView = findViewById(R.id.rootView);
+            Animation shake = AnimationUtils.loadAnimation(this,R.anim.shake);
+            Drawable incorrectBackground = getDrawable(R.drawable.incorrect_edit_text);
+            passwordInput.setBackground(incorrectBackground);
+            passwordInput.startAnimation(shake);
+            Snackbar snackbar = Snackbar.make(contextView, R.string.login_error, Snackbar.LENGTH_SHORT);
+            snackbar.getView().setBackgroundColor(Color.parseColor("#ff0000"));
+            TextView snackbarTextView = snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+            snackbarTextView.setTextSize( 16 );
+            snackbar.show();
+        }
+    }
 
     public void openSignupForm(View view)
     {
