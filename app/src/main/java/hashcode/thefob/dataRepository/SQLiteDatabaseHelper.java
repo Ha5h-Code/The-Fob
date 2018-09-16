425c09d60c00458d8a4c3b4bd9138c2af6481b1d
Nipun Sampath <nipunsampath@gmail.com>
2018-09-16 13:47:40 +0530

    changed database helper class

--- app/src/main/java/hashcode/thefob/dataRepository/SQLiteDatabaseHelper.java
+++ app/src/main/java/hashcode/thefob/dataRepository/SQLiteDatabaseHelper.java
@@ -26,7 +26,7 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    static public synchronized SQLiteDatabaseHelper getInstance(Context context) {
+    static private synchronized SQLiteDatabaseHelper getInstance(Context context) {
         if (instance == null) {
             instance = new SQLiteDatabaseHelper(context);
         }
@@ -45,17 +45,7 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper
         onCreate(sqLiteDatabase);
     }
 
-    public void changePasswords(Context context,String oldPassword,String newPassword)
-    {
-        final SQLiteDatabaseHelper helper = new SQLiteDatabaseHelper(context);
-        final SQLiteDatabase db = helper.getWritableDatabase(oldPassword);
 
-
-        String PRAGMA_KEY = "PRAGMA key = " + oldPassword + ";";
-        String PRAGMA_REKEY = "PRAGMA rekey = " + newPassword + ";";
-        db.rawExecSQL(PRAGMA_KEY);
-        db.rawExecSQL(PRAGMA_REKEY);
-    }
 
     public static SQLiteDatabase connectDataBase(Context context,String password)
     {
